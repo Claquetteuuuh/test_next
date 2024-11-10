14121d6c3b84910bac92fbe9473d800e79a8ea79
Claquetteuuuh <th.biabiany.dev@gmail.com>
2024-11-10 10:19:12 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes apagnan.</li>
+          <li>Save and see your changes QUOICOUBEH.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
